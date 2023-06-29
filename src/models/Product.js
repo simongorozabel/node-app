02b7c8bd36a4adf7494d9d73e6dbe4d1ef37f4e7
Simon Gorozabel <sgorozabel@gmail.com>
2023-06-28 19:46:12 -0500

    advances

--- src/models/Product.js
+++ src/models/Product.js
@@ -3,13 +3,18 @@ const sequelize = require("../utils/connection");
 // En Mayúsculas y singular      // en minúsculas y singular
 const Product = sequelize.define("product", {
   // Definimos las columnas aquí
-  columna1: {
+  brand: {
     type: DataTypes.STRING,
-    allowNull: false,
   },
-  columna2: {
+  model: {
     type: DataTypes.STRING(100),
     // allowNull por defecto esta en true
+  },
+  year: {
+    type: DataTypes.INTEGER,
+  },
+  color: {
+    type: DataTypes.STRING,
   },
 });
 
